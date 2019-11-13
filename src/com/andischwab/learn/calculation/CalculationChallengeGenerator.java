ddee4a04d2a6f3c555ade136a3e04dafb490b4d9
Andreas Schwab <schwab@apt-solutions.de>
2019-11-13 15:34:24 +0100

    bugfix of iteration numbers

--- src/com/andischwab/learn/calculation/CalculationChallengeGenerator.java
+++ src/com/andischwab/learn/calculation/CalculationChallengeGenerator.java
@@ -2,7 +2,7 @@ package com.andischwab.learn.calculation;
 
 public class CalculationChallengeGenerator {
 	
-	public static final int DEFAULT_NUMBER_CHALLENGES = 1000;
+	public static final int DEFAULT_NUMBER_CHALLENGES = 200;
 	public static final int DEFAULT_MAX_NUMBER = 10;
 	
 	
@@ -12,7 +12,7 @@ public class CalculationChallengeGenerator {
 		int numberOfChallenges = DEFAULT_NUMBER_CHALLENGES;
 		int maxNumber = DEFAULT_MAX_NUMBER;
 				
-		for(int i = 1; i < numberOfChallenges; i++) {
+		for(int i = 1; i <= numberOfChallenges; i++) {
 			System.out.println(CalcChallenge.generateChallenge(maxNumber, null));
 		}
 	}
